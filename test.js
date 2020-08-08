94f62594d0c788a161c90a68ac53ba5e535f4774
ryan farber <32393930+ryanfarber@users.noreply.github.com>
2020-08-07 19:42:05 -0700

    updated test.js

--- test.js
+++ test.js
@@ -2,8 +2,6 @@
 
 const Logger = require("./index.js");
 const logger = new Logger({filename: "test.js", save: false});
-const timestamp = require("time-stamp");
-let ts = timestamp("HH:mm:ss.ms");
 
 logger.log("hello world")
 logger.info("hello info")
